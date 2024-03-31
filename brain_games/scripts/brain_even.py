24c2b6a7fcbac204981b10475a3dd586f9f87fc0
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-03-31 14:36:50 +0700

    Make linter happy

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,6 +4,7 @@ from brain_games.games.even import generate_questions
 
 
 def main():
-    game_description = 'Answer "yes" if the number is even, otherwise answer "no".'
+    game_description = 'Answer "yes" if the number is '\
+        'even, otherwise answer "no".'
     questions = generate_questions()
     engine.run(game_description, questions)
