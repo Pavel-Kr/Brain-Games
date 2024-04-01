e71b1a21c7344e93945e081c514b69d6b31e0125
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-04-01 19:26:26 +0700

    Refactored question generation for progression game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,22 +1,26 @@
 import random
 
 
+def generate_question():
+    progression_len = 10
+    start = random.randint(1, 20)
+    step = random.randint(1, 5)
+    elem = start
+    missing_elem = random.randint(0, progression_len - 1)
+    progression = []
+    for j in range(progression_len):
+        if j == missing_elem:
+            progression.append("..")
+            correct_answer = str(elem)
+        else:
+            progression.append(str(elem))
+        elem += step
+    question = " ".join(progression)
+    return (question, correct_answer)
+
+
 def generate_questions(questions_count=3):
     questions = []
     for i in range(questions_count):
-        progression_len = 10
-        start = random.randint(1, 20)
-        step = random.randint(1, 5)
-        elem = start
-        missing_elem = random.randint(0, progression_len - 1)
-        progression = []
-        for j in range(progression_len):
-            if j == missing_elem:
-                progression.append("..")
-                correct_answer = str(elem)
-            else:
-                progression.append(str(elem))
-            elem += step
-        question = " ".join(progression)
-        questions.append((question, correct_answer))
+        questions.append(generate_question())
     return questions
